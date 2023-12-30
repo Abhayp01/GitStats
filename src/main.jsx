fc4b5665b1cdb273d9efa52ecfac88da62227874
Abhayp01 <prajapatiabhay2003@gmail.com>
2023-12-30 21:15:12 +0530

    routes change2

--- src/main.jsx
+++ src/main.jsx
@@ -5,33 +5,22 @@ import ReactDOM from "react-dom/client";
 import Root from "./Root.jsx";
 import Contact from "./components/contact.jsx";
 import User from "./components/User.jsx";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 
-const router = createBrowserRouter(
-  createRoutesFromElements([
-    <Route path="/" element={<Root />}>
-      <Route path="/" element={<App />}></Route>
-      <Route path="/about" element={<Blog />}></Route>
-      <Route path="/contact" element={<Contact />}></Route>
-      <Route path="/user/:userid" element={<User />}></Route>
-    </Route>,
-    <Route path="/about" element={<Root />}>
-      <Route path="/about" element={<Blog />}></Route>
-    </Route>,
-    <Route path="/user/:userid" element={<Root />}>
-      <Route path="/user/:userid" element={<User />}></Route>
-    </Route>,
-  ])
-);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/user/:userid" element={<User />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
