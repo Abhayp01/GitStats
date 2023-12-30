719f9a2d724337952f731982c7b399e08f329b5f
Abhayp01 <prajapatiabhay2003@gmail.com>
2023-12-30 21:04:03 +0530

    changes in route

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import App from './App.jsx'
-import Blog from './components/blog.jsx';
+import React from "react";
+import App from "./App.jsx";
+import Blog from "./components/blog.jsx";
 import ReactDOM from "react-dom/client";
-import Root from './Root.jsx';
-import Contact from './components/contact.jsx';
-import User from './components/User.jsx';
+import Root from "./Root.jsx";
+import Contact from "./components/contact.jsx";
+import User from "./components/User.jsx";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
+import "./index.css";
 
 const router = createBrowserRouter(
-  
-  createRoutesFromElements(
-    <Route path='/' element={<Root/>}>
-      <Route path='/' element={<App/>}></Route>
-      <Route path='/about' element={<Blog/>}></Route>
-      <Route path='/contact' element={<Contact/>}></Route>
-      <Route path='/user/:userid' element={<User/>}></Route>
-
-    </Route>
-  )
-
+  createRoutesFromElements([
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/about" element={<Blog />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/user/:userid" element={<User />}></Route>
+    </Route>,
+    <Route path="/about" element={<Root />}>
+      <Route path="/about" element={<Blog />}></Route>
+    </Route>,
+    <Route path="/user/:userid" element={<Root />}>
+      <Route path="/user/:userid" element={<User />}></Route>
+    </Route>,
+  ])
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
